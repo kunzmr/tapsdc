23f98a7062cc0820c0e5788a17a598bb8e0cd44b
Ross Kunz <rosskunz@gmail.com>
2024-10-03 09:39:06 -0400

    bug fixes with initial values

--- tapsdc/R/fit_sdc.R
+++ tapsdc/R/fit_sdc.R
@@ -1,62 +1,61 @@
 #' Fit the Standard Diffusion Curve
 #'
-#' Fit the standard diffusion curve coefficients given a flux (or a series of flux) and time array.
+#' Fit the standard diffusion curve coefficients given a y (or a series of y) and time array.
 #' This function uses the Levenberg-Marquardt algorithm for a non-linear least squares fit of the coefficients.
 #'
-#' @param time_array An array of time values in which the flux was collected.
-#' @param flux A matrix or array of flux measurements.
+#' @param x An array of time values in which the y was collected.
+#' @param y A matrix or array of flux measurements.
 #' @param n_max An integer for the total number of attempts for the LM-optimization.
-#' @param fixed_values A list of fixed parameters. For example, assuming the flux is irreversible, then desorption must be zero, i.e., fixed_values = list(desorption = 0).
+#' @param fixed_values A list of fixed parameters. For example, assuming the y is irreversible, then desorption must be zero, i.e., fixed_values = list(kd = 0).
 #' @param n_cores An integer for the total number cores to use in calculation.
 #' @return The standard diffusion curve based on the kinetic coefficients given.
 #' @importFrom parallel detectCores makeCluster parLapply stopCluster
 #' @examples
-#' time_array = (1:5000) * 0.001
+#' x = (1:5000) * 0.001
 #'
-#' flux = sdc(time_array) + rnorm(length(time_array), mean = .2, sd = .01)
-#' fit = fit_single_sdc(flux, time_array = time_array, fixed_values = list(desorption = 0))
+#' y = sdc(x) + rnorm(length(x), mean = .2, sd = .01)
+#' fit = fit_single_sdc(x = x, y = y, fixed_values = list(desorption = 0))
 #'
-#' flux2 = sdc(time_array) + rnorm(length(time_array), mean = .1, sd = .05)
-#' flux = as.data.frame(cbind(flux, flux2))
-#' names(flux) = c("f1", "f2")
+#' y2 = sdc(x) + rnorm(length(x), mean = .1, sd = .05)
+#' y = as.data.frame(cbind(y, y2))
+#' names(y) = c("f1", "f2")
 #'
-#' fit = fit_sdc(time_array, flux, fixed_values = list(desorption = 0))
+#' fit = fit_sdc(x, y, fixed_values = list(desorption = 0))
 #' names(fit)
-#' plot(fit$data$flux)
-#' lines(fit$data$fitted, col = "red")
+#' plot(fit$data$x, fit$data$y)
+#' lines(fit$data$x, fit$data$fitted, col = "red")
 #'
 #' @export fit_sdc
-fit_sdc = function(time_array, flux, n_max = 1, fixed_values = NULL,
-                   n_cores = NULL) {
+fit_sdc = function(x, y, n_max = 1, fixed_values = NULL, n_cores = NULL) {
 
 
-  if (is.null(dim(flux))) {
-    result = tapsdc::fit_single_sdc(time_array = time_array, flux = flux, n_max = n_max)
+  if (is.null(dim(y))) {
+    result = tapsdc::fit_single_sdc(x = x, y = y, n_max = n_max)
   } else {
 
-    flux_list = vector(mode = "list", length = ncol(flux))
-    for (i in 1:ncol(flux)) {
-      flux_list[[i]] = flux[, i]
+    y_list = vector(mode = "list", length = ncol(y))
+    for (i in 1:ncol(y)) {
+      y_list[[i]] = y[, i]
     }
 
     if (is.null(n_cores)) {
       n_cores = parallel::detectCores() - 1
     }
     cl = parallel::makeCluster(n_cores)
-    results = parallel::parLapply(cl, flux_list, tapsdc::fit_single_sdc,
-                                  time_array = time_array,
+    results = parallel::parLapply(cl, y_list, tapsdc::fit_single_sdc,
+                                  x = x,
                                   fixed_values = fixed_values, n_max = n_max)
     parallel::stopCluster(cl)
 
     result = list(
       data = NULL,
       coefs = NULL,
-      nls_model = vector(mode = "list", length = ncol(flux)),
-      ls_model = vector(mode = "list", length = ncol(flux))
+      nls_model = vector(mode = "list", length = ncol(y)),
+      ls_model = vector(mode = "list", length = ncol(y))
     )
     for (i in 1:length(results)) {
-      results[[i]]$data$index = colnames(flux)[i]
-      results[[i]]$coefs$index = colnames(flux)[i]
+      results[[i]]$data$index = colnames(y)[i]
+      results[[i]]$coefs$index = colnames(y)[i]
 
       if (i == 1) {
         result$data = results[[i]]$data
--- tapsdc/R/fit_single_sdc.R
+++ tapsdc/R/fit_single_sdc.R
@@ -3,77 +3,84 @@
 #' Fit the standard diffusion curve coefficients given a flux and time array.
 #' This function uses the Levenberg-Marquardt algorithm for a non-linear least squares fit of the coefficients.
 #'
-#' @param time_array An array of time values in which the flux was collected.
-#' @param flux An array of flux measurements.
+#' @param x An array of time values in which the flux was collected.
+#' @param y An array of flux measurements.
 #' @param n_max An integer for the total number of attempts for the LM-optimization.
 #' @param fixed_values A list of fixed parameters. For example, assuming the flux is irreversible, then desorption must be zero, i.e., fixed_values = list(desorption = 0).
 #' @return A list of results including the time series data as a dataframe, the optimized coefficients as a dataframe, the non-linear least squares fit of the coefficients, and the least squares fit of the sdc to the original data.
 #' @importFrom pracma trapz
 #' @importFrom minpack.lm nlsLM
 #' @examples
-#' time_array = (1:5000) * 0.001
-#' flux = sdc(time_array) + rnorm(length(time_array), mean = .2, sd = .01)
-#' fit = fit_single_sdc(flux, time_array = time_array, fixed_values = list(desorption = 0))
+#' x = (1:5000) * 0.001
+#' flux = sdc(x) + rnorm(length(x), mean = .2, sd = .01)
+#' fit = fit_single_sdc(y, x = x, fixed_values = list(desorption = 0))
 #'
 #' plot(fit$data$time_array, fit$data$flux)
 #' lines(fit$data$time_array, fit$data$fitted, col = "red")
 #'
 #' @export fit_single_sdc
-fit_single_sdc = function(time_array, flux, n_max = 1, fixed_values = NULL) {
+fit_single_sdc = function(x, y, n_max = 1, fixed_values = NULL) {
   result = list(
     data = data.frame(
-      time_array = time_array,
-      flux = flux,
-      fitted = rep(0, length(flux)),
-      sdc = rep(0, length(flux))
+      x = x,
+      y = y,
+      fitted = rep(0, length(y)),
+      sdc = rep(0, length(y))
     ),
     coefs = data.frame(
-      intercept = 0,
+      baseline = 0,
       beta = 0,
-      diffusion = 0,
-      adsorption = 0,
-      desorption = 0
+      tau = 0,
+      ka = 0,
+      kd = 0
     ),
     nls_model = NULL,
     ls_model = NULL
   )
 
   # ensure the values of the flux non-negative
-  flux = flux - min(flux)
+  y = y - min(y)
   # area normalize the flux
-  flux = flux / pracma::trapz(time_array, flux)
-  time_step = median(diff(time_array))
+  y = y / pracma::trapz(x, y)
+  time_step = median(diff(x))
 
   # Estimate baseline and area
-  fit_ar = lm(flux[-1] ~ flux[-length(flux)])
+  #fit_ar = lm(y[-1] ~ y[-length(y)])
 
-  coefs = list(
-    intercept = (median(flux) + coef(fit_ar)[1]) / 2,
-    beta = max(c(time_step, coef(fit_ar)[2])),
-    diffusion = 1, adsorption = 1, desorption = 0
+  #noise_term = rnorm(length(y), 0, sd(diff(y)))
+  #fit_noise = lm(y ~ noise_term)
+
+  init_area = pracma::trapz(x, y - median(y))
+  init = list(
+    baseline = median(y), # fit_noise$coefficients[1], #as.numeric((median(y) + coef(fit_ar)[1]) / 2),
+    beta = init_area, # as.numeric(max(c(time_step, coef(fit_ar)[2]))),
+    tau = 1 / (6 * x[which.max(y)]), # pracma::trapz(x, x * y),
+    ka = abs(pracma::trapz(x, x * (y - median(y)))),
+    kd = 0
   )
 
-  if (!is.null(fixed_values)) {
-    for (i in names(fixed_values)) {
-      idx = which(names(coefs) == i)
-      if (length(idx) != 0) {
-        coefs = coefs[-idx]
-        assign(i, fixed_values[[i]])
-        result$coefs[[i]] = fixed_values[[i]]
-      }
+  str_form = c("y ~ ", "baseline", " + ", "beta",
+               " * tapsdc::sdc(x, ", "tau", ", ",
+               "ka", ", ", "kd", ")")
+
+
+  for (i in names(fixed_values)) {
+    init_idx = which(names(init) == i)
+    form_idx = which(str_form == i)
+    if (length(form_idx) != 0) {
+      init = init[-init_idx]
+      str_form[form_idx] = paste0(str_form[form_idx], " = ", fixed_values[[i]])
+      result$coefs[[i]] = fixed_values[[i]]
     }
   }
+  str_form = paste0(str_form, collapse = "")
 
   fit_list = vector(mode = "list", length = n_max)
   fit_rmse = rep(1e6, n_max)
+
   for (i in 1:n_max) {
-    fit_list[[i]] = try(
-      minpack.lm::nlsLM(
-        flux ~ intercept + beta * tapsdc::sdc(time_array, diffusion,
-                                           adsorption, desorption),
-        start = coefs
-      ),
-      silent = TRUE
+    fit_list[[i]] = try(minpack.lm::nlsLM(formula(str_form), start = init),
+                        silent = TRUE
     )
     if (class(fit_list[[i]]) == "nls") {
       fit_rmse[i] = sqrt(mean(summary(fit_list[[i]])$residuals^2))
@@ -84,14 +91,14 @@ fit_single_sdc = function(time_array, flux, n_max = 1, fixed_values = NULL) {
 
   if (class(nls_model) == "nls") {
     for (i in names(coef(nls_model))) {
-      if (i %in% c("diffusion", "adsorption", "desorption")) {
+      if (i %in% c("tau", "ka", "kd")) {
         result$coefs[[i]] = abs(coef(nls_model)[[i]])
       }
     }
-    result$data$sdc = sdc(result$data$time_array, result$coefs$diffusion,
-                          result$coefs$adsorption, result$coefs$desorption)
-    ls_model = lm(result$data$flux ~ result$data$sdc)
-    result$coefs$intercept = coef(ls_model)[1]
+    result$data$sdc = sdc(result$data$x, result$coefs$tau,
+                          result$coefs$ka, result$coefs$kd)
+    ls_model = lm(result$data$y ~ result$data$sdc)
+    result$coefs$baseline = coef(ls_model)[1]
     result$coefs$beta = coef(ls_model)[2]
     result$data$fitted = ls_model$fitted.values
     result$nls_model = nls_model
